bfb3b36736286bd2f9e8053b597a46322a26fe81
Tania Prysiazhnyk <73783964+Tanchyyk@users.noreply.github.com>
2021-02-27 12:33:39 +0200

    add put_users_on_map.py

---
+++ put_users_on_map.py
@@ -0,0 +1,14 @@
+import folium
+from folium.plugins import MarkerCluster
+
+
+def put_users_on_map(list_of_users: list):
+    users_map = folium.Map(tiles="Stamen Terrain", location=[49.8397, 24.0297], zoom_start=4)
+    marker_cluster = folium.plugins.MarkerCluster().add_to(users_map)
+    for user in list_of_users:
+        folium.Marker(location=[user[-1][0], user[-1][1]],
+                                          radius=10,
+                                          popup=user[0],
+                                          color='green',
+                                          fill_opacity=0.5).add_to(marker_cluster)
+    return users_map
