ba540644c7ccaacf01e93a56c3d604c01f836cea
Tania Prysiazhnyk <73783964+Tanchyyk@users.noreply.github.com>
2021-02-27 12:32:46 +0200

    add get_jsons.py

---
+++ get_jsons.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def get_json(user_name):
+    base_url = "https://api.twitter.com/1.1/friends/list.json"
+    bearer_token = "AAAAAAAAAAAAAAAAAAAAAKkSNAEAAAAADGjajQeyVyQTi28qCAff1Mrl520%3DGzjvYM2YJgXGIMZQzXE23E4jUcSmIsOpsGCQcAnhL87so6O4JC"
+
+    search_headers = {
+        "Authorization": f'Bearer {bearer_token}'
+    }
+    search_params = {
+        "screen_name": user_name,
+        "count": 4
+    }
+
+    response = requests.get(base_url, headers=search_headers, params=search_params)
+    return response.json()
